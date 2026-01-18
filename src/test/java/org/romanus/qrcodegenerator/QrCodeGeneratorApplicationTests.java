dc3e5c15e7bad09c9125f1261aa933c1294ebc41
Roman Prokhorovich <pomaprokhorovich583@gmail.com>
2026-01-18 15:40:54 +0400

    Initial commit

---
+++ src/test/java/org/romanus/qrcodegenerator/QrCodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.romanus.qrcodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
